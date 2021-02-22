e851ac5750829c98e2daa5749eee6c4fd7847139
dashmaster-c <77778981+dashmaster-c@users.noreply.github.com>
2021-02-22 14:25:55 +0900

    Update TDmain.java

--- src/touhouturret/core/TDmain.java
+++ src/touhouturret/core/TDmain.java
@@ -3,12 +3,20 @@ package touhouturret.core;
 import mindustry.mod.Mod;
 import mindustry.content.*;
 import mindustry.entities.*;
+
 import mindustry.world.*;
 import mindustry.world.modules.*;
 import mindustry.world.consumers.*;
 import mindustry.world.draw.*;
 import mindustry.world.meta.*;
 import mindustry.world.blocks.*;
+
+import mindustry.type.*;
+import mindustry.ctype.*;
+
+import mindustry.ui.*;
+import mindustry.ui.layout.*;
+import mindustry.ui.fragments.*;
 
 public class TDmain extends Mod{
 
